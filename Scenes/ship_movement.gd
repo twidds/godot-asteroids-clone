7725b00ea57c01a2051c8ce68d7efa99d6f474e4
Mark <>
2026-05-31 21:30:25 -0700

    fix turnaround logic. refactor movement script. cap speed at 1500

--- Scenes/ship_movement.gd
+++ Scenes/ship_movement.gd
@@ -3,7 +3,7 @@ extends Node
 #Hooks to drop in the object being controlled
 @export var ship : PhysicsBody2D
 const ACCEL_RATE := 10 #Pixels/tick^2
-const MAX_VELOCITY := 5000.0 #Pixels/tick
+const MAX_VELOCITY := 1500.0 #Pixels/tick
 const TURN_RATE := 5.0 #Rad/tick?
 
 var velocity: Vector2
@@ -17,33 +17,30 @@ func _physics_process(delta: float) -> void:
 	if not ship:
 		pass
 	
-	var turn_direction = Input.get_axis("p1_move_left","p1_move_right")
-	var accel_dir = Input.get_axis("p1_accelerate","p1_brake")
+	var turn_direction: = Input.get_axis("p1_move_left","p1_move_right")
+	var accel_dir := Input.get_axis("p1_brake","p1_accelerate")
 	
 	#Turn ship
-	var turn_amt = turn_direction * TURN_RATE * delta
-	ship.rotate(turn_amt)
+	var turn_amt: = 0.0
+	if accel_dir < 0.0: #point ship oppposite to velocity
+		var turn_towards := (velocity * -1).angle()
+		var diff := angle_difference(ship.rotation, turn_towards)
+		turn_direction = -1.0 if diff < 0.0 else 1.0
+		
+		turn_amt = TURN_RATE * turn_direction * delta
+		if abs(diff) < abs(turn_amt):
+			turn_amt = diff
+		ship.rotate(turn_amt)
+	else: #Turn based on L/R
+		turn_amt = turn_direction * TURN_RATE * delta
+		ship.rotate(turn_amt)
 	
 	#Accelerate ship
-	var accel = Vector2.ZERO
-	var ship_pointing = Vector2.from_angle(ship.rotation).rotated(PI/2)
-	if accel_dir > 0.0: #braking turn
-		var turn_towards := (velocity).angle()
-		var diff := angle_difference(turn_towards, ship_pointing.angle())
-		
-		
-		print(diff)
-		turn_direction = 1.0 if diff < 0.0 else -1.0
-		
-		if abs(diff) < TURN_RATE * delta:
-			turn_amt = abs(diff) * turn_direction * delta
-		else:
-			turn_amt = TURN_RATE * turn_direction * delta
-		ship.rotate(turn_amt)
-	else:
-		accel = accel_dir * ship_pointing * ACCEL_RATE
+	if accel_dir >= 0.0:
+		var accel := accel_dir * Vector2.from_angle(ship.rotation) * ACCEL_RATE
+		velocity += accel
+		if velocity.length() > MAX_VELOCITY:
+			velocity = velocity.normalized() * MAX_VELOCITY
 	
-	velocity += accel
-	if velocity.length() > MAX_VELOCITY:
-		velocity = velocity.normalized() * MAX_VELOCITY
 	ship.position += velocity * delta
+	print(velocity.length())
